58848ac5fab9f67def780decdbaafbb79712784e
Hargobind S. Khalsa <khalsah@gmail.com>
2010-05-22 15:37:54 -0600

    Correct URLs in php scripts

--- php/nav.php
+++ php/nav.php
@@ -1,5 +1,5 @@
-<a href="/history.php">History</a> | 
-<a href="/allhistory.php">All History</a> |
+<a href="history.php">History</a> | 
+<a href="allhistory.php">All History</a> |
 <a href="userhistory.php">History by User</a> |
-<a href="/score.php">Score</a> | 
-<a href="/sections.php">Sections</a>
+<a href="score.php">Score</a> | 
+<a href="sections.php">Sections</a>
