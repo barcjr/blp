2d3fa553d134d0b22232480dbf8d344e6bf05502
Hargobind S. Khalsa <khalsah@gmail.com>
2010-06-26 22:52:03 -0600

    Add list of missing sections to section.php report

--- php/sections.php
+++ php/sections.php
@@ -1,30 +1,30 @@
 <html>
 <head>
-<title>BLP - PHP - Sections</title><meta http-equiv="refresh" content="10" />
+    <title>BLP - PHP - Sections</title><meta http-equiv="refresh" content="10" />
 
 </head>
 <body>
-<div id="title"><b>Sections Contacted</b></div>
-<div>
-<br />
+    <div id="title"><b>Sections Contacted</b></div>
+    <div>
+        <br />
 
-<?php require('nav.php'); ?>
+        <?php require('nav.php'); ?>
 
-<br /> <br />
+        <br /> <br />
 
-<?php
-require_once('database.php');
+        <?php
+    require_once('database.php');
 
-$query = "SELECT Section.FullName, UNIX_TIMESTAMP(MIN(Contact.Time)) FROM Section
-JOIN Station ON( Station.SectionID = Section.SectionID )
+    $query = "SELECT Section.FullName, UNIX_TIMESTAMP(MIN(Contact.Time)) FROM Section
+    JOIN Station ON( Station.SectionID = Section.SectionID )
 JOIN Contact ON( Contact.StationID = Station.StationID )
 GROUP BY Section.SectionID;";
 
 $db_result = mysql_query($query) or die(mysql_error());
 
 while($row= mysql_fetch_array($db_result)) {
-	if($row[1] > (time()-60))
-		echo "<font color='#009900'>$row[0]</font><br />";
+    if($row[1] > (time()-60))
+        echo "<font color='#009900'>$row[0]</font><br />";
 }
 echo "<br />";
 
@@ -41,10 +41,32 @@ echo "<div id='count'>You have contacted - <b>$count</b></div>";
 <?php
 while($row = mysql_fetch_array($db_result)) {
 
-  echo "<li>" . $row[0] . "</li>\n";
+    echo "<li>" . $row[0] . "</li>\n";
 
- }
+}
 ?>
 </ul>
+<?php
+$query = "SELECT DISTINCT Section.FullName FROM Section
+WHERE NOT Section.SectionID in
+(
+SELECT DISTINCT Station.SectionID FROM Contact
+JOIN Station ON (Contact.StationID = Station.StationID)
+)
+ORDER BY Section.FullName;";
+$db_result = mysql_query($query) or die(mysql_error());
+$count = mysql_num_rows($db_result);
+echo "<div id='count'>You have <b>NOT</b> contacted - <b>$count</b></div>";
+?>
+<ul>
+<?php
+while($row = mysql_fetch_array($db_result)) {
+
+    echo "<li>" . $row[0] . "</li>\n";
+
+}
+?>
+</ul>
+
 </body>
 </html>
